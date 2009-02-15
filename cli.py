e1f248bfefe7afb1f8ce3d604c67796d69841a58
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-02-14 22:27:18 -0200

    Refactoring to test the CLI parsing.

--- cli.py
+++ cli.py
@@ -2,22 +2,29 @@ from optparse import OptionParser
 
 class CLI(object):
 
-    def start(self):
-        parser = OptionParser()
+    def __init__(self):
+        self.__config_parser()
 
-        parser.add_option("-v", "--version", 
+    def __config_parser(self):
+        self.__parser = OptionParser()
+
+        self.__parser.add_option("-v", "--version", 
                 dest="schema_version", 
                 default=None, 
                 help="Schema version to migrate to. If not provided will migrate to the last version available.")
                 
-        parser.add_option("-c", "--config", 
+        self.__parser.add_option("-c", "--config", 
                 dest="db_config_file", 
                 default="simple-db-migrate.conf", 
                 help="Use specific config file. If not provided, will use simple-db-migrate.conf that is located in the current directory.")
                 
-        parser.add_option("-d", "--dir", 
+        self.__parser.add_option("-d", "--dir", 
                 dest="migrations_dir", 
                 default=".", 
                 help="Find migration files in a specific directory. If not provided will search for files in the current directory.")
 
-        return parser.parse_args()
+    def get_parser(self):
+        return self.__parser
+
+    def parse(self):
+        self.__parser.parse_args()
---
+++ tests/cli_test.py
@@ -0,0 +1,22 @@
+from _test_env import *
+from cli import CLI
+import unittest
+
+class CLITest(unittest.TestCase):
+
+    def test_it_should_configure_all_options(self):
+        cli = CLI()
+        parser = cli.get_parser()
+        
+        self.assertTrue(parser.has_option("-h"))
+        self.assertTrue(parser.has_option("--help"))
+        self.assertTrue(parser.has_option("-c"))
+        self.assertTrue(parser.has_option("--config"))
+        self.assertTrue(parser.has_option("-d"))
+        self.assertTrue(parser.has_option("--dir"))
+        self.assertTrue(parser.has_option("-v"))
+        self.assertTrue(parser.has_option("--version"))
+
+
+if __name__ == "__main__":
+    unittest.main()
