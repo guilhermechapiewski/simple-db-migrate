3a37bc1ca87c5f2d766c0bd577f9fb1f177a4bbf
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-02-15 02:26:22 -0300

    #8: First version of the instalation.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "SimpleDBMigrate",
+    version = "1.0",
+    packages = find_packages("src"),
+    package_dir = {"":"src"},
+    include_package_data = True,
+    
+    install_requires = ["mysql>=2.0"],
+
+    author = "Guilherme Chapiewski",
+    author_email = "guilherme.chapiewski@gmail.com",
+    description = "simple-db-migrate is a database versioning and migration tool inspired on Rails Migrations.",
+    license = "Apache License 2.0",
+    keywords = "database migration tool mysql",
+    url = "http://simple-db-migrate.lighthouseapp.com",
+    long_description = "simple-db-migrate is a database versioning and migration tool inspired on Rails Migrations. This tool helps you easily refactor, manage and track your database schema. The main difference is that Rails migrations are intended to be used only on Ruby projects while simple-db-migrate makes it possible to have migrations in any language (Java, Python, Ruby, PHP, whatever). This is possible because simple-db-migrate uses database's DDL (Data Definition Language) to do the database operations, while Rails Migrations are written in a Ruby internal DSL."
+    download_url = "http://github.com/guilhermechapiewski/simple-db-migrate"
+)
+
