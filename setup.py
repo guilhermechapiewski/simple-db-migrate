4f2d17a81fac88ea21d6a42afe6182e31fb889c8
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-03-11 15:27:20 -0300

    Changing version number for release 1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "SimpleDBMigrate",
-    version = "1.1",
+    version = "1.2",
     packages = find_packages("src"),
     package_dir = {"":"src"},
     scripts = ["src/db-migrate"],
