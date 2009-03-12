e092b774a644b1a34b410aaf66dce06819e6e604
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-03-12 02:11:42 -0300

    Changing package name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "SimpleDBMigrate",
+    name = "simple-db-migrate",
     version = "1.2",
     packages = find_packages("src"),
     package_dir = {"":"src"},
@@ -14,7 +14,7 @@ setup(
     description = "simple-db-migrate is a database versioning and migration tool inspired on Rails Migrations.",
     license = "Apache License 2.0",
     keywords = "database migration tool mysql",
-    url = "http://simple-db-migrate.lighthouseapp.com",
+    url = "http://guilhermechapiewski.github.com/simple-db-migrate/",
     long_description = "simple-db-migrate is a database versioning and migration tool inspired on Rails Migrations. This tool helps you easily refactor, manage and track your database schema. The main difference is that Rails migrations are intended to be used only on Ruby projects while simple-db-migrate makes it possible to have migrations in any language (Java, Python, Ruby, PHP, whatever). This is possible because simple-db-migrate uses database's DDL (Data Definition Language) to do the database operations, while Rails Migrations are written in a Ruby internal DSL.",
     download_url = "http://github.com/guilhermechapiewski/simple-db-migrate",
 )
