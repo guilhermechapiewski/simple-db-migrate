481fcdf741a37b9943cfafc19ba3ff39f8421bb6
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-03-17 15:42:26 -0300

    Preparing for release version v1.2.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "simple-db-migrate",
-    version = "1.2",
+    version = "1.2.1",
     packages = find_packages("src"),
     package_dir = {"":"src"},
     scripts = ["src/db-migrate"],
