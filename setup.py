556584c3e810f856ca1dbf8e5ff41374096358d1
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-05-08 16:21:32 -0300

    Critical bug corrected. Migrations were not working when files contained non-ascii characters.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "simple-db-migrate",
-    version = "1.2.5",
+    version = "1.2.6",
     packages = find_packages("src"),
     package_dir = {"":"src"},
     scripts = ["src/db-migrate"],
