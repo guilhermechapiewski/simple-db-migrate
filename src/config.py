040e397f0eb0707d4ffbf023a9df3a22be62a168
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-10-06 15:25:21 -0300

    Config working again with sample settings.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import codecs
 import os
 import sys
+import re
 
 class Config(object):
     
@@ -36,12 +37,12 @@ class FileConfig(Config):
     
     class SettingsFile(object):
         @staticmethod
-        def get_file_modules(filename):
+        def get_file_imports(filename):
             imports = []
             try:
                 f = open(filename)
                 for line in f:
-                    line = line.strip()
+                    line = line.rstrip()
                     if line.startswith('import') or line.startswith('from'):
                         imports.append(line)
             finally:
@@ -49,47 +50,63 @@ class FileConfig(Config):
             return imports
         
         @staticmethod
-        def import_file_modules(filename):
-            modules = FileConfig.SettingsFile.get_file_modules(filename)
-            for module in modules:
-                exec(module)
-        
+        def exec_import(import_line, global_variables, local_variables):
+            if import_line.startswith('import'):
+                modules = import_line.lstrip('import').split(',')
+                for module in modules:
+                    __import__(module.strip(), global_variables, local_variables, [], -1)
+            else:
+                match = re.search(r'from (.*) import (.*)', import_line)
+                modules = [m.strip() for m in match.groups()[1].split(',')]
+                __import__(match.groups()[0], global_variables, local_variables, modules, -1)
+                
         @staticmethod
         def import_file(full_filename):
             path, filename = os.path.split(full_filename)
             
+            # add settings dir from path
             sys.path.insert(0, path)
+            
             # read imports from config file
-            try:
-                FileConfig.SettingsFile.import_file_modules(full_filename)
-            except Exception, e:
-                raise Exception("error executing imports from config file '%s': %s" % (filename, str(e)))
+#            try:
+#                modules = FileConfig.SettingsFile.get_file_imports(full_filename)
+#                for module in modules:
+#                    FileConfig.SettingsFile.exec_import(module, globals(), locals())
+#            except Exception, e:
+#                raise Exception("error executing imports from config file '%s': %s" % (filename, str(e)))
+
             # read config file
+            local_variables = locals()
+            local_variables['__file__'] = full_filename
             try:
-                execfile(full_filename, locals(), globals())
+                execfile(full_filename, globals(), local_variables)
             except IOError:
                 raise Exception("%s: file not found" % filename)
             except Exception, e:
                 raise Exception("error interpreting config file '%s': %s" % (filename, str(e)))
+
+            # remove settings dir from path
             sys.path.remove(path)
+            
+            return local_variables
     
     def __init__(self, config_file="simple-db-migrate.conf"):
         self._config = {}
         self.put("db_version_table", self.DB_VERSION_TABLE)
         
-        # read configurations
-        FileConfig.SettingsFile.import_file(config_file)
+        # read configuration
+        settings = FileConfig.SettingsFile.import_file(config_file)
         
         config_path = os.path.split(config_file)[0]
-        self.put("db_host", self.get_local_variable(locals(), 'DATABASE_HOST', 'HOST'))
-        self.put("db_user", self.get_local_variable(locals(), 'DATABASE_USER', 'USERNAME'))
-        self.put("db_password", self.get_local_variable(locals(), 'DATABASE_PASSWORD', 'PASSWORD'))
-        self.put("db_name", self.get_local_variable(locals(), 'DATABASE_NAME', 'DATABASE'))
+        self.put("db_host", self.get_variable(settings, 'DATABASE_HOST', 'HOST'))
+        self.put("db_user", self.get_variable(settings, 'DATABASE_USER', 'USERNAME'))
+        self.put("db_password", self.get_variable(settings, 'DATABASE_PASSWORD', 'PASSWORD'))
+        self.put("db_name", self.get_variable(settings, 'DATABASE_NAME', 'DATABASE'))
         
-        migrations_dir = self.get_local_variable(locals(), 'DATABASE_MIGRATIONS_DIR', 'MIGRATIONS_DIR')
+        migrations_dir = self.get_variable(settings, 'DATABASE_MIGRATIONS_DIR', 'MIGRATIONS_DIR')
         self.put("migrations_dir", self._parse_migrations_dir(migrations_dir, config_path))
         
-    def get_local_variable(self, local_variables_dict, name, old_name):
+    def get_variable(self, local_variables_dict, name, old_name):
         if name in local_variables_dict or old_name in local_variables_dict:
             return local_variables_dict.get(name, local_variables_dict.get(old_name))
         else:
