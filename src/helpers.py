6aea328d602e0cf57cf928f06d661f49a9b285ed
Wandenberg Peixoto <wandenberg@nl159282.(none)>
2010-09-30 01:26:36 -0300

    removendo imports nao utilizados

--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,16 @@
-import re
-
 class Lists(object):
-    
+
     @classmethod
     def subtract(self, list_a, list_b):
         return [l for l in list_a if l not in list_b]
-        
+
 class Utils(object):
-    
+
     @classmethod
     def how_many(self, string, match):
         if not match or len(match) != 1:
             raise Exception("match should be a char")
         count = {}
         for char in string:
-        	count[char] = count.get(char, 0) + 1
+            count[char] = count.get(char, 0) + 1
         return count.get(match, 0)
