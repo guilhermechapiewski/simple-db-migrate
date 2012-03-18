fbb578913833ae9525a29c7d7d30c533d2ce7b2d
Wandenberg Peixoto <wandenberg@gmail.com>
2012-03-18 15:33:41 -0300

    refactor on Config class to use keys in lowercase and simplifying FileConfig initialization

--- simple_db_migrate/config.py
+++ simple_db_migrate/config.py
@@ -10,20 +10,25 @@ class Config(object):
 
     def __init__(self, inital_config=None):
         self._config = inital_config or {}
+        for key in self._config.keys():
+            self._config[key.lower()] = self._config[key]
 
     def __repr__(self):
         return str(self._config)
 
     #default_value was assigned as !@#$%&* to be more easy to check when the default value is None, empty string or False
     def get(self, config_key, default_value='!@#$%&*'):
+        config_key = config_key.lower()
         return Config._get(self._config, config_key, default_value)
 
     def put(self, config_key, config_value):
+        config_key = config_key.lower()
         if config_key in self._config:
             raise Exception("the configuration key '%s' already exists and you cannot override any configuration" % config_key)
         self._config[config_key] = config_value
 
     def update(self, config_key, config_value):
+        config_key = config_key.lower()
         if config_key in self._config:
             value = self.get(config_key)
             self.remove(config_key)
@@ -32,6 +37,7 @@ class Config(object):
 
     def remove(self, config_key):
         try:
+            config_key = config_key.lower()
             del self._config[config_key]
         except KeyError:
             raise Exception("invalid configuration key ('%s')" % config_key)
@@ -61,64 +67,22 @@ class Config(object):
 class FileConfig(Config):
 
     def __init__(self, config_file="simple-db-migrate.conf", environment=''):
-        self._config = {}
-
         # read configuration
         settings = Utils.get_variables_from_file(config_file)
 
-        self.put("database_host", FileConfig._get_variable(settings, 'DATABASE_HOST', environment=environment))
-        self.put("database_user", FileConfig._get_variable(settings, 'DATABASE_USER', environment=environment))
-        self.put("database_password", FileConfig._get_variable(settings, 'DATABASE_PASSWORD', environment=environment))
-        self.put("database_name", FileConfig._get_variable(settings, 'DATABASE_NAME', environment=environment))
+        super(FileConfig, self).__init__(inital_config=settings)
 
-        self.put("database_engine", FileConfig._get_variable(settings, 'DATABASE_ENGINE', 'mysql', environment))
-        self.put("database_version_table", FileConfig._get_variable(settings, 'DATABASE_VERSION_TABLE', self.DB_VERSION_TABLE, environment))
-
-        self.put("utc_timestamp", ast.literal_eval(str(FileConfig._get_variable(settings, 'UTC_TIMESTAMP', 'False', environment))))
-
-        self._get_custom_variables(settings, ['DATABASE_HOST', 'DATABASE_USER', 'DATABASE_PASSWORD', 'DATABASE_NAME', 'DATABASE_ENGINE', 'DATABASE_VERSION_TABLE', 'DATABASE_MIGRATIONS_DIR'], environment=environment)
-        self._get_general_variables(settings, environment=environment)
-
-        migrations_dir = FileConfig._get_variable(settings, 'DATABASE_MIGRATIONS_DIR', environment=environment)
-        config_dir = os.path.split(config_file)[0]
-        self.put("database_migrations_dir", FileConfig._parse_migrations_dir(migrations_dir, config_dir))
-
-    def _get_custom_variables(self, settings, default_variables = [], environment=''):
-        """
-        Put on config var all custom variables present in config file wich starts with DATABASE_
-        """
-        p = re.compile("^DATABASE_(?P<name>.*)")
-        for key in settings.keys():
-            m_key = p.match(key)
-            if m_key and key not in default_variables:
-                self.put("db_%s" % m_key.group('name').lower(), FileConfig._get_variable(settings, key,  m_key.group('name'), environment=environment))
-
-    def _get_general_variables(self, settings, environment=''):
-        """
-        Put on config var all variables present in config file wich not starts with DATABASE_
-        """
-        p = re.compile("^(DATABASE_.*|os|MIGRATIONS_DIR|migrations_dir|UTC_TIMESTAMP|utc_timestamp)$")
-        for key in settings.keys():
-            m_key = p.match(key)
-            if not m_key:
-                self.put(key.lower(), FileConfig._get_variable(settings, key, None, environment=environment))
-
-    #default_value was assigned as !@#$%&* to be more easy to check when the default value is None, empty string or False
-    @staticmethod
-    def _get_variable(settings, name, default_value='!@#$%&*', environment=''):
         if environment:
-            try:
-                return FileConfig._get_variable(settings, environment.upper() + "_" + name if name else None)
-            except Exception, e:
-                pass
+            prefix = environment + "_"
+            for key in self._config.keys():
+                if key.startswith(prefix):
+                    self.update(key[len(prefix):], self.get(key))
 
-        try:
-            return FileConfig._get(settings, name)
-        except Exception, e:
-            if (default_value != '!@#$%&*'):
-                return default_value
+        self.update("database_engine", self.get("database_engine", 'mysql'))
+        self.update("database_version_table", self.get("database_version_table", self.DB_VERSION_TABLE))
 
-            if environment:
-                raise Exception("invalid keys ('%s_%s', '%s')" % (environment.upper(), name, name))
+        self.update("utc_timestamp", ast.literal_eval(str(self.get("utc_timestamp", 'False'))))
 
-            raise Exception("invalid key ('%s')" % (name))
+        migrations_dir = self.get("database_migrations_dir")
+        config_dir = os.path.split(config_file)[0]
+        self.update("database_migrations_dir", FileConfig._parse_migrations_dir(migrations_dir, config_dir))
