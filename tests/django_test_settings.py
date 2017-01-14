b3d9c53e6be5498322d8811c34a340da805eda3e
Wandenberg <wandenberg@gmail.com>
2017-01-14 21:25:48 +0100

    adjustments to run tests with django 1.10.5

--- tests/django_test_settings.py
+++ tests/django_test_settings.py
@@ -1,3 +1,9 @@
 INSTALLED_APPS = [
-    'simple_db_migrate.db_migrate',
+    'simple_db_migrate.db_migrate'
 ]
+SECRET_KEY = 'secret_key'
+
+import django
+
+if hasattr(django, 'setup'):
+    django.setup()
